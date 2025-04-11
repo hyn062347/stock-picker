e2dae76ec3d46cb2a2d26ea3f68c7b4fb350166f
BENTRO <hyn062347@gmail.com>
2025-04-12 01:04:29 +0900

    Update Try.py

--- server/Try.py
+++ server/Try.py
@@ -16,7 +16,7 @@ DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_DATABASE")
+    "database": os.getenv("DB_NAME")
 }
 
 #실행시 심볼 초기화
