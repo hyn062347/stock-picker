aee915339deb1ed8bff039fadd4fc1288754046f
BENTRO <hyn062347@gmail.com>
2025-04-12 01:12:25 +0900

    Local Test Complete

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,6 +1,6 @@
-import { Pool } from 'pg';
+import mysql from "mysql2/promise";
 
-const pool = new Pool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
